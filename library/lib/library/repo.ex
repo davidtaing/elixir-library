dbfbdd3b28c1f882efcb7ebcefea85b34db59303
David Taing <dave@teeang.net>
2024-08-12 21:19:13 +1000

    initial commit

---
+++ library/lib/library/repo.ex
@@ -0,0 +1,9 @@
+defmodule Library.Repo do
+  use AshPostgres.Repo,
+    otp_app: :library
+
+  def installed_extensions do
+    # Add extensions here, and the migration generator will install them.
+    ["ash-functions"]
+  end
+end
